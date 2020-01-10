1b3f570add24deb71ecec2c55a6360975fca2c23
Your Name <scott.bell@gmail.com>
2020-01-10 07:51:38 +0000

    Avg, max, min

--- main.c
+++ main.c
@@ -42,8 +42,51 @@ void swap(int* a, int* b) {
     *b = t;
 }
 
+int* get (struct Array *arr, int index) {
+    if (index >= 0 && index < arr->length) {
+        return &arr->A[index];
+    }
+}
+
+// O(n)
+int max(struct Array *arr) {
+    int max = arr->A[0];
+    for (int i = 0; i < arr->length; i++)
+        if (arr->A[i] > max)
+            max = arr->A[i];
+    return max;
+}
+
+// O(n)
+int min(struct Array* arr) {
+    int min = arr->A[0];
+    for (int i = 0; i < arr->length; i++)
+        if (arr->A[i] < min)
+            min = arr->A[i];
+    return min;
+}
+
+// O(n)
+int sum(struct Array* arr) {
+    int sum = 0;
+    for (int i = 0; i < arr->length; i++)
+        sum += arr->A[i];
+    return sum;
+}
+
+int avg(struct Array* arr) {
+    return sum(arr)/arr->length;
+}
+
+void set (struct Array* arr, int index, int val) {
+    if (index >= 0 && index < arr->length) {
+        arr->A[index] = val;
+    }
+}
+
 // binary search, O(log n)
-int bin_search(struct Array *arr, int key) {
+// pre-condition: arr is sorted
+int bin_search(struct Array* arr, int key) {
     int low = 0;
     int high = arr->length-1;
 
@@ -62,11 +105,11 @@ int bin_search(struct Array *arr, int key) {
 }
 
 // linear search O(n)
-int find(struct Array *arr, int key) {
+int find(struct Array* arr, int key) {
     for (int i = 0; i < arr->length; i++) {
         if (arr->A[i] == key) {
             if (i>0)
-                swap(&arr->A[i],&arr->A[i-1]);
+                swap(&arr->A[i],&arr->A[i-1]); // transposition
             return i;
         }
     }
@@ -75,8 +118,8 @@ int find(struct Array *arr, int key) {
 
 int main() {
     struct Array arr={{4, 8, 10, 15, 18, 21, 24, 27, 29, 33, 34, 37, 39, 41, 43},20,15};
-    display(&arr);
-    printf("%d", bin_search(&arr, 43));
+
+    printf("%d\n", avg(&arr));
 
 
     return 0;
